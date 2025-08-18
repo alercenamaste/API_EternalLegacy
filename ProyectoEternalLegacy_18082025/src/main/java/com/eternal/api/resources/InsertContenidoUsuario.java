4ff7f2b08e49f17a85a7da1aa16500e2ea116a51
alercenamaste <alercenamaste@gmail.com>
2025-08-18 18:05:13 -0400

    Add files via upload

---
+++ ProyectoEternalLegacy_18082025/src/main/java/com/eternal/api/resources/InsertContenidoUsuario.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eternal.api.resources;
+
+/**
+ *
+ * @author mauricio.olguin
+ */
+public class InsertContenidoUsuario {
+    
+     private String URL;
+    private String email;
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}

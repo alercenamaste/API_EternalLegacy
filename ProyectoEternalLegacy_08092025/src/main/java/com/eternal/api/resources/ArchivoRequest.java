3b289e5a697446b43fc75c8cb954ca81ae0415cf
alercenamaste <alercenamaste@gmail.com>
2025-09-08 18:47:58 -0300

    Add files via upload

---
+++ ProyectoEternalLegacy_08092025/src/main/java/com/eternal/api/resources/ArchivoRequest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eternal.api.resources;
+
+/**
+ *
+ * @author mauricio.olguin
+ */
+public class ArchivoRequest {
+     private String fileName;
+    private String fileType;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+    
+}
---
+++ ProyectoEternalLegacy_08092025/src/main/java/com/eternal/api/resources/RequestLeeDtos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eternal.api.resources;
+
+/**
+ *
+ * @author mauricio.olguin
+ */
+public class RequestLeeDtos {
+    
+    String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}

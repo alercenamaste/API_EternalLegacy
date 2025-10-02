8f07235948b6b158ff17252b760a9a6095be2f41
alercenamaste <alercenamaste@gmail.com>
2025-10-02 15:57:52 -0300

    Add files via upload

---
+++ ProyectoEternalLegacy_02102025/src/main/java/com/eternal/api/AppConfig.java
@@ -0,0 +1,11 @@
+package com.eternal.api;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/api/v1")
+public class AppConfig extends ResourceConfig {
+    public AppConfig() {
+        packages("com.eternal.api.resources");
+    }
+}
